1aec4c62736a768c79fd1fccb0b8a135c16dfc1d
Mikko Olkkonen <mikkoolkkonen@Mikko-MacBook-Air.local>
2024-12-06 18:12:38 +0200

    8 exercises for part 6

---
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -0,0 +1,46 @@
+import { useDispatch, useSelector } from 'react-redux'
+import PropTypes from 'prop-types'
+import { vote } from '../reducers/anecdoteReducer'
+
+const Anecdote = ({ anecdote, handleClick }) => {
+  return(
+    <div>
+      <div>
+        {anecdote.content}
+      </div>
+      <div>
+        has {anecdote.votes}
+        <button onClick={handleClick}>vote</button>
+      </div>
+    </div>
+  )
+}
+
+const AnecdoteList = () => {
+  const dispatch = useDispatch()
+  const anecdotes = useSelector(state => state)
+
+  return(
+    <div>
+      {[...anecdotes].sort((a, b) => b.votes - a.votes).map(anecdote =>
+        <Anecdote
+          key={anecdote.id}
+          anecdote={anecdote}
+          handleClick={() => {
+          dispatch(vote(anecdote.id))
+        }} />
+      )}
+    </div>
+  )
+}
+
+Anecdote.propTypes = {
+  anecdote: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
+    votes: PropTypes.number.isRequired
+  }).isRequired,
+  handleClick: PropTypes.func.isRequired
+}
+
+export default AnecdoteList
